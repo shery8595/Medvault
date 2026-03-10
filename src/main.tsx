45c42326ae8a4cc4adf9648ce1b123516362aef7
shehryar rehman <131074050+shery8595@users.noreply.github.com>
2026-03-11 01:19:04 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
+// @ts-ignore
+if (window.LOG_DEBUG) window.LOG_DEBUG('main.tsx: Starting execution...');
+
 import './polyfills';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// @ts-ignore
+if (window.LOG_DEBUG) window.LOG_DEBUG('main.tsx: Mounting React...');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+
+// @ts-ignore
+if (window.LOG_DEBUG) window.LOG_DEBUG('main.tsx: Render call finished.');
