bd4fa232a45bcd781b250521ef95b852f47f5048
shehryar rehman <131074050+shery8595@users.noreply.github.com>
2026-03-11 00:53:07 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig(({ mode }) => {
     ],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      global: 'globalThis',
     },
     resolve: {
       alias: {
