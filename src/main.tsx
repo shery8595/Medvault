fa1da50b1a18b5d861d72ebc321fa16369e4542b
shehryar rehman <131074050+shery8595@users.noreply.github.com>
2026-03-11 00:52:51 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import { Buffer } from 'buffer';
+import process from 'process';
+
+window.Buffer = Buffer;
+window.process = process;
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
